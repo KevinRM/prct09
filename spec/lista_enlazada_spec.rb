c5711528b3e9dd2feb969d6ce0eeee4a51f572cf
Kevin <alu0100789461@ull.edu.es>
2015-11-17 16:52:37 +0000

    Prueba para insertar elementos en la lista

--- spec/lista_enlazada_spec.rb
+++ spec/lista_enlazada_spec.rb
@@ -18,4 +18,22 @@ describe ListaEnlazada do
       expect(@objeto_nodo.next).to eq(nil)
     end
   end
+  
+  context "Pruebas de Lista" do
+    before :all do
+      @nodo_1 = Node.new(1,nil,nil)
+      @nodo_2 = Node.new(2,nil,nil)
+      @nodo_3 = Node.new(3,nil,nil)
+      @nodo_4 = Node.new(4,nil,nil)
+      @nodo_5 = Node.new(5,nil,nil)
+            
+      @lista = List.new(nil)
+    end
+        
+    it "Insercion de un elemento" do
+      @lista.add_nodo(@nodo_1)
+        
+      expect(@lista.cabeza).to eq(@nodo_1)
+    end
+  end
 end
