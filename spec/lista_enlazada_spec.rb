a4afff599753f9ebff58a4730d2e4fe43d3259b8
Kevin <alu0100789461@ull.edu.es>
2015-11-17 16:40:10 +0000

    Añadiendo pruebas de nodo

--- spec/lista_enlazada_spec.rb
+++ spec/lista_enlazada_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe ListaEnlazada do
-  it 'has a version number' do
-    expect(ListaEnlazada::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context "Node" do
+    before :all do
+      @objeto_nodo = Node.new(1)
+    end
+        
+    it "Debe existir un Nodo de la lista con sus datos" do
+      expect(@objeto_nodo.value).to eq(1)
+    end
+    
+    it "Debe existir un Nodo de la lista con su anterior" do
+      expect(@objeto_nodo.prev).to eq(nil)
+    end
+        
+    it "Debe existir un Nodo de la lista con su siguiente" do
+      expect(@objeto_nodo.next).to eq(nil)
+    end
   end
 end
