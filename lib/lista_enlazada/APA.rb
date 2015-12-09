cca8c1f55c1979e57de160cf92f8189cf2f43252
Kevin <alu0100789461@ull.edu.es>
2015-12-09 20:03:46 +0000

    Añadiendo codigo para obtener autores

--- lib/lista_enlazada/APA.rb
+++ lib/lista_enlazada/APA.rb
@@ -6,4 +6,17 @@ class Apa < List
     def add_elementos(x)
         @lista.add_varios(x)
     end
+    
+    def get_autores()
+        valor_nodo = @lista.get_nodo()
+        autores = valor_nodo.get_autor()
+        array_devolver = []
+        i = 0
+        autores.each do |val|
+            autor = val.split(' ')
+            array_devolver[i] = autor[1] + " " + autor[0]
+            i += 1
+        end
+        return array_devolver
+    end
 end
